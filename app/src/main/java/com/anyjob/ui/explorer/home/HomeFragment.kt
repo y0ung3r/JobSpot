27fe078e990cbcb9e710d6f10066ac0fc475c79b
Никита Великий <mtyoungstz@outlook.com>
2022-05-30 01:54:21 +0500

    Подключен Google Maps

--- app/src/main/java/com/anyjob/ui/explorer/home/HomeFragment.kt
+++ app/src/main/java/com/anyjob/ui/explorer/home/HomeFragment.kt
@@ -5,14 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.anyjob.R
 import com.anyjob.databinding.FragmentHomeBinding
 import com.anyjob.ui.explorer.home.viewModels.HomeViewModel
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
     private val _viewModel by viewModel<HomeViewModel>()
     private lateinit var _binding: FragmentHomeBinding
+
+    private val _mapView by lazy {
+        childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+    }
 
     private fun onMapReady(googleMap: GoogleMap) {
         googleMap.uiSettings.isMyLocationButtonEnabled = true
@@ -21,7 +27,9 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        _binding.map.getMapAsync(::onMapReady)
+
+        _mapView.getMapAsync(::onMapReady)
+
         return _binding.root
     }
 }
