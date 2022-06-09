3c8cf31ef53cd364aa7d35f228d952891acff285
Никита Великий <mtyoungstz@outlook.com>
2022-06-10 01:13:14 +0500

    Добавлен макет фрагмента настроек

--- app/src/main/java/com/anyjob/ui/controls/bottomSheets/AddressesBottomSheetDialog.kt
+++ app/src/main/java/com/anyjob/ui/controls/bottomSheets/AddressesBottomSheetDialog.kt
@@ -1,8 +1,14 @@
 package com.anyjob.ui.controls.bottomSheets
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.Resources
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import com.anyjob.databinding.AddressesBottomSheetBinding
+import com.anyjob.ui.extensions.afterTextChanged
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class AddressesBottomSheetDialog(context: Context, theme: Int) : BottomSheetDialog(context, theme) {
@@ -10,8 +16,15 @@ class AddressesBottomSheetDialog(context: Context, theme: Int) : BottomSheetDial
         AddressesBottomSheetBinding.inflate(layoutInflater)
     }
 
+    private fun onAddressChanged(address: String) {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(_binding.root)
+
+        _binding.bottomSheetLayout.minimumHeight = Resources.getSystem().displayMetrics.heightPixels
+        _binding.addressField.afterTextChanged(::onAddressChanged)
     }
 }
