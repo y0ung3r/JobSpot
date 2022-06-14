0965528833828b1ff9e951d6d43f1fa314764ba6
Никита Великий <mtyoungstz@outlook.com>
2022-06-15 02:10:59 +0500

    Изменен код, расширяющий AddressesBottomSheetDialog до его содержимого

--- app/src/main/java/com/anyjob/ui/explorer/search/controls/bottomSheets/addresses/AddressesBottomSheetDialog.kt
+++ app/src/main/java/com/anyjob/ui/explorer/search/controls/bottomSheets/addresses/AddressesBottomSheetDialog.kt
@@ -5,10 +5,14 @@ import android.content.res.Resources
 import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import com.anyjob.R
 import com.anyjob.databinding.AddressesBottomSheetBinding
 import com.anyjob.ui.explorer.search.controls.bottomSheets.addresses.adapters.AddressesAdapter
 import com.anyjob.ui.explorer.search.controls.bottomSheets.addresses.models.UserAddress
 import com.anyjob.ui.extensions.afterTextChanged
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -56,11 +60,21 @@ class AddressesBottomSheetDialog(
         }
     }
 
+    private fun setupFullHeight() {
+        val bottomSheet = findViewById<View>(R.id.design_bottom_sheet)
+
+        if (bottomSheet != null) {
+            val layoutParams = bottomSheet.layoutParams
+            layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT
+            bottomSheet.layoutParams = layoutParams
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(_binding.root)
+        setupFullHeight()
 
-        _binding.bottomSheetLayout.minimumHeight = Resources.getSystem().displayMetrics.heightPixels
         _binding.addressField.afterTextChanged(::onAddressChanged)
 
         _addressesAdapter = AddressesAdapter(
